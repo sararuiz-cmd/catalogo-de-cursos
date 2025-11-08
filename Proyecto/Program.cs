2dff8e60cc699ae44cb5c78c8b07dc51403810be
Sara Ruiz <svruiz@uamv.edu.ni>
2025-11-08 15:37:08 -0600

    Cree la clase base

---
+++ Proyecto/Program.cs
@@ -0,0 +1,22 @@
+﻿// Clase base 
+using System;
+
+namespace CatalogoCursos
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Area { get; set; }
+        public string Descripcion { get; set; }
+
+        public Curso(int id, string nombre, string area, string descripcion)
+        {
+            Id = id;
+            Nombre = nombre;
+            Area = area;
+            Descripcion = descripcion;
+        }
+    }
+}
+
